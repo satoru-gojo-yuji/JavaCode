b89ce61eb207817e564d7ba7290e4cdc3581782a
Abhishek-94244 <abhishekpal94244@gmail.com>
2023-07-12 23:48:58 +0530

    New push

---
+++ src/main/java/OOPS/Abstraction.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public abstract class Abstraction {
+
+     abstract void M1();
+
+     int r ;
+     Abstraction()
+     {
+          System.out.println("Ab");
+     }
+
+}
+class A extends Abstraction
+{
+     A()
+     {
+          super();
+     }
+     void M1()
+     {
+          System.out.println("Abhishek");
+     }
+
+}
+class Test
+{
+     public static void main(String[] args)
+     {
+          A r = new A();
+          r.M1();
+     }
+}
