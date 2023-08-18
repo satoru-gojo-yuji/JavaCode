b9558e00a34477302e8673bdf0b3f28cfe140a41
Abhishek pal <Abhishekpal94244@gmail.com>
2023-08-18 22:46:45 +0530

    New

--- src/main/java/JavaCode/String/Revers_string.java
+++ src/main/java/JavaCode/String/Revers_string.java
@@ -1,4 +1,18 @@
 package JavaCode.String;
 
 public class Revers_string {
+    public static void main(String[] args)
+    {
+        String t = "Abhishek Pal";
+
+        char ch =t.charAt(4);
+        //  System.out.println(ch);
+
+        char chr[] =t.toCharArray();
+
+        for(int i =chr.length-1; i>=0;  i--)
+        {
+            System.out.print(chr[i]);
+        }
+    }
 }
