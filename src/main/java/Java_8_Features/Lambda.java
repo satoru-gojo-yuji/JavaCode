a0b235da2fada59cffbcf6d879e4284de3b65181
Abhishek Pal <Abhishekpal94244@gmail.com>
2023-12-04 22:26:49 +0530

    New update

--- src/main/java/Java_8_Features/Lambda.java
+++ src/main/java/Java_8_Features/Lambda.java
@@ -5,16 +5,15 @@ import java.sql.SQLOutput;
 public class Lambda {
     public static void main(String[] args) {
 
-
-        public void m1()
+      /*  public void m1()
         {
             System.out.println("New methods");
-        }
+        }*/
 
         // Converted into lambda expressions
         // NO method name, return type, access modifier
 
-        () -> System.out.println("New methods");
+       // () -> System.out.println("New methods");
 
     }
 }
